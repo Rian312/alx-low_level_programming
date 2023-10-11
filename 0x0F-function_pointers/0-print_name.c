00ab1a33d7bdc66fcee4462cf568240add91c0a3
rian312 <brianmasinde78@gmail.com>
2023-10-11 18:42:17 +0300

    oggi

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
  * print_name - prints a name
  * @name: the name to print
  * @f: pointer to the function
- * 
+ *
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name !=NULL && f !=NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
